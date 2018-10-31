53b1ade459c7e415ed9af2d1e257de2143ee4a31
msphonex <msphonex@sina.com>
2018-10-31 17:50:17 +0800

    new

--- extract_pwd.py
+++ extract_pwd.py
@@ -7,10 +7,19 @@ source_file = '/home/unknown/individual/python/test.txt'
 
 target_file = '/home/unknown/individual/python/user_and_pwd.txt'
 
-f_s = file(source_file)
+f_s = open(source_file)
 
-f_t = file(target_file, 'w')
+f_t = open(target_file, 'w')
 
 while True:
     line = f_s.readline()
-    if 'ssglog' in line:
+    if len(line) == 0:
+        break
+    if 'sshlog' in line:
+        line_list = line.split()
+        if len(line_list) == 7:
+            f_t.write(line_list[6]+'\n')
+        else:
+            f_t.write(line_list[6]+' '+line_list[7]+'\n')
+f_s.close
+f_t.close
